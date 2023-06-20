600b1611f6983905df133edeb1c5c2a8de6e6bea
vamsi-4-3-4 <94716145+vamsi-4-3-4@users.noreply.github.com>
2023-06-20 16:30:37 +0530

    Create scatter_plot_example_2_seaborn.py

---
+++ scatter_plot_example_2_seaborn.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+print("--DATASETS GIVEN IN THE SEABORN--\n",sns.get_dataset_names())
+iris=sns.load_dataset("iris")
+print("-----IRIS DATASET HEAD-----\n",iris.head())
+anagrams=sns.load_dataset("anagrams")
+carcrashes=sns.load_dataset("car_crashes")
+print("-----CAR CRASHES DATASET-----------\n",carcrashes.head())
+print("-----ANAGRAMS DATASET HEAD----\n",anagrams.head())
+print("---IRIS DATASET----\n",iris.count())
+print("-----ANAGRAMS DATASET----\n",anagrams.count())
+print("A FIGURE LEVEL FUNCTION CONTROLS THE FIGURE CATPLOT,RELPLOT")
+fig,ax=plt.subplots()
+sns.scatterplot(x="petal_length",y="petal_width",hue="species",data=iris)
+plt.show()
